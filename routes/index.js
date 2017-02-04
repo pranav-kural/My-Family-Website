ba86e4f1000cd921b2bae0e0c1d70a5c3c57f5fa
Pranav Kural <pranavkural042@gmail.com>
2017-02-03 20:17:52 -0500

    added commenting to index.js file

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// route to my page
 router.get('/pranav', function(req, res, next) {
   res.render('pranav', { title: 'Pranav Kural' });
 });
 
+// route to my sister's page
 router.get('/aanchal', function(req, res, next) {
   res.render('aanchal', { title: 'Aanchal Sharma' });
 });
 
+// route to my mom's page
 router.get('/anu', function(req, res, next) {
   res.render('anu', { title: 'Anuradha Sharma' });
 });
 
+// route to my dad's page
 router.get('/tarloki', function(req, res, next) {
   res.render('tarloki', { title: 'Tarloki Nath Sharma' });
 });
