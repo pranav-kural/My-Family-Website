fbe32222f81e7fba76509f220d0fc1b86962cbd5
Pranav Kural <pranavkural042@gmail.com>
2017-02-03 19:58:16 -0500

    added the route for pranav

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/pranav', function(req, res, next) {
+  res.render('pranav', { title: 'Pranav Kural' });
+});
+
 module.exports = router;
