da7b0b6b786338a1c2535ec8fa74461269b7fe9c
Pranav Kural <pranavkural042@gmail.com>
2017-02-03 22:28:55 -0500

    updated the files

--- routes/index.js
+++ routes/index.js
@@ -8,22 +8,22 @@ router.get('/', function(req, res, next) {
 
 // route to my page
 router.get('/pranav', function(req, res, next) {
-  res.render('pranav', { title: 'Pranav Kural' });
+  res.render('pranav', { title: 'Pranav Kural', description: 'I\'m a computer programming student studying at Georgian College.' });
 });
 
 // route to my sister's page
 router.get('/aanchal', function(req, res, next) {
-  res.render('aanchal', { title: 'Aanchal Sharma' });
+  res.render('aanchal', { title: 'Aanchal Sharma', description: '' });
 });
 
 // route to my mom's page
 router.get('/anu', function(req, res, next) {
-  res.render('anu', { title: 'Anuradha Sharma' });
+  res.render('anu', { title: 'Anuradha Sharma', description: '' });
 });
 
 // route to my dad's page
 router.get('/tarloki', function(req, res, next) {
-  res.render('tarloki', { title: 'Tarloki Nath Sharma' });
+  res.render('tarloki', { title: 'Tarloki Nath Sharma', description: '' });
 });
 
 module.exports = router;
